731e58bb62b20b7c09ef14035c93282c507d49bb
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-09-18 15:35:09 -0500

    fix connection detailjs

---
+++ assets/constants/connectionDetail.js
@@ -0,0 +1,9 @@
+//connection to database
+const connectionDetail = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employee_db',
+  };
+  
+  module.exports = connectionDetail;
