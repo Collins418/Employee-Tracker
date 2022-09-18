311ed622cd1234e3a6aecd7d32d8ff183234a954
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-09-18 17:51:46 -0500

    rolecontroller

---
+++ assets/tableController/rolecontroller.js
@@ -0,0 +1,22 @@
+const tableController = require('./tablecontroller.js');
+
+//For custom queries of table: role
+const roleController = new TableController('role');
+
+roleController.selectJoinDepartment = async function(departmentTableName) {
+  try {
+    this.checkConnection();
+    const joinQuery = `
+    SELECT ${this.name}.id AS ID, ${this.name}.title AS Title,
+      ${this.name}.salary AS Salary, ${departmentTableName}.name AS Department
+    FROM ${this.name}
+    LEFT JOIN ${departmentTableName} ON ${this.name}.department_id = ${departmentTableName}.id`;
+    const [roles] = await this.connection.query(joinQuery);
+    return roles;
+  } catch(error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+module.exports = roleController;
