a287aef888fe8f9bce3eb4e389b1ac7bdfee1dbe
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-09-21 19:59:50 -0500

    fix employee area in seed

--- sql/seed.sql
+++ sql/seed.sql
@@ -7,23 +7,23 @@ INSERT INTO department(name)
         ('Legal');
         
 INSERT INTO role(title, salary, department_id)
-	VALUES('Sales Lead', 100000, 1),
-		('Salesperson', 80000, 1),
-        ('Lead Engineer', 150000, 2),
-        ('Software Engineer', 125000, 2),
-        ('Account Manager', 300000, 3),
-        ('Accountant', 250000, 3),
+	VALUES('Sales Lead', 120000, 1),
+		('Salesperson', 90000, 1),
+        ('Lead Engineer', 160000, 2),
+        ('Software Engineer', 128000, 2),
+        ('Account Manager', 310000, 3),
+        ('Accountant', 200000, 3),
         ('Legal Team Lead', 250000, 4),
-        ('Lawyer', 190000, 4);
+        ('Lawyer',180000, 4);
 
--- Managers
+-- Manager area
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 	VALUES('Robbin', 'Collins', 1, NULL),
 		('Austin', 'Smith', 3, NULL),
         ('Ryan', 'Avery', 5, NULL),
         ('Jennifer', 'Sweet', 7, NULL);
 
--- Employees
+-- Employees area
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 	VALUES('Angela', 'Castro', 2, 1),
 	('Faezana', 'Ali', 4, 2),
